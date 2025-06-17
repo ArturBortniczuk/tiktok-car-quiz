1bee9cecb103bc1e44435f300e0bbd6476e6ee7d
ArturBortniczuk <a.bortniczuk@grupaeltron.pl>
2025-06-17 17:39:58 +0200

    Update page.tsx

---
+++ src/app/result/page.tsx
@@ -0,0 +1,8 @@
+export default function ResultPage() {
+  // TODO: Implement result page
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <p>Result page - coming soon!</p>
+    </div>
+  );
+}
